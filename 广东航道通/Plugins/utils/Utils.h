771f3fd7bddac8ee6324dcafe443be90dfc1ee33
zhaowanfeng <81502731@qq.com>
2019-04-25 00:19:46 +0800

    rr

---
+++ 广东航道通/Plugins/utils/Utils.h
@@ -0,0 +1,17 @@
+//
+//  Utils.h
+//  广东航道通
+//
+//  Created by wanger on 2019/4/24.
+//
+
+#ifndef Utils_h
+#define Utils_h
+
+
+#endif /* Utils_h */
+
+@interface Utils : NSObject
+  +(NSString *)jsonStrConvertByObject:(NSMutableDictionary *)dict;
+  +(BOOL) isBlankString:(NSString *)string;
+@end
